9982e17a199200380c12bbddb22df38656bb46f3
Patrick Kissling <patrick.kissling@gmx.de>
2021-01-14 19:13:48 +0100

    Hello world

--- src/config/env_vars.rs
+++
@@ -1,5 +0,0 @@
-use std::env;
-
-pub fn expected_api_key() -> String {
-    env::var("API_KEY").expect("'API_KEY' not set")
-}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,3 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-mod routes;
-mod from_request;
-mod config;
-mod services;
-
-#[macro_use]
-extern crate rocket;
-
-use crate::config::env_vars;
-use rocket::config::{Config, Environment};
-
 fn main() {
-
-    ensure_preconditions();
-
-    let routes = routes![routes::update_container::update_container];
-    let base_url = "/";
-
-    let config = Config::build(Environment::Production)
-        .address("0.0.0.0")
-        .port(5000)
-        .finalize()
-        .expect("failed");
-
-
-    // TODo docker system prune -a
-    // TODO container not gracefully stopping!
-    rocket::custom(config)
-        .mount(base_url, routes)
-        .launch();
-}
-
-fn ensure_preconditions() {
-    env_vars::expected_api_key();
+    println!("Hello world!")
 }
